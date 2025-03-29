a54c45127e685292e0b946013eb2b801650b3bb5
junqing233 <2959159031@qq.com>
2025-03-29 21:28:54 +0800

    Add files via upload

--- AncientChineseMythology.cs
+++ AncientChineseMythology.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 using System.IO;
 using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.Localization;
+using Terraria.ModLoader.IO;
+using Microsoft.Xna.Framework;
+using AncientChineseMythology.NPCs.Boss;
 
 namespace AncientChineseMythology
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
-	public class AncientChineseMythology : Mod
-	{
-		public override void HandlePacket(BinaryReader reader, int whoAmI)
+    // Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+    public class AncientChineseMythology : Mod
+    {
+        public override void HandlePacket(BinaryReader reader, int whoAmI)
         {
             AncientChineseMythologyMessageType msgType = (AncientChineseMythologyMessageType)reader.ReadByte();
             switch (msgType)
@@ -39,10 +44,95 @@ namespace AncientChineseMythology
             }
         }
 
-	}
+    }
 
-	public enum AncientChineseMythologyMessageType : byte
+    public enum AncientChineseMythologyMessageType : byte
     {
         SyncGrowthPlayer
+    }
+
+    public class BossChecklistIntegration : ModSystem
+    {
+        private static readonly Version BossChecklistAPIVersion = new Version(1, 6); // 版本设置
+
+        public override void PostSetupContent()
+        {
+            DoBossChecklistIntegration();
+        }
+
+        private void DoBossChecklistIntegration()
+        {
+            if (!ModLoader.TryGetMod("BossChecklist", out Mod bossChecklist) || bossChecklist.Version < BossChecklistAPIVersion)
+            {
+                return; // 如果未找到模组或版本不匹配
+            }
+            //第1个Boss第1个Boss第1个Boss第1个Boss第1个Boss
+            //***********************************************************************************************************************************
+            string internalName = "BlackBear"; // 唯一标识符
+            float weight = 0.1f; // 权重
+            Func<bool> downed = () => DownedBossSystem.downedBlackBear; // Boss 击败状态
+
+            int bossType = ModContent.NPCType<BlackBear>(); // Boss 的 NPC 类型
+            //int spawnItem = ModContent.ItemType<Items.MyBoss1Summoner>(); // 召唤 Boss 的物品类型
+
+            //List<int> collectibles = new List<int>()
+            //{
+               
+            //};
+            // 自定义图标显示法
+            var customPortrait = (SpriteBatch sb, Rectangle rect, Color color) =>
+            {
+                Texture2D texture = ModContent.Request<Texture2D>("AncientChineseMythology/Textures/BlackBear/BlackBear").Value;
+                Vector2 centered = new Vector2(rect.X + (rect.Width / 2) - (texture.Width / 2), rect.Y + (rect.Height / 2) - (texture.Height / 2));
+                sb.Draw(texture, centered, color);
+            };
+
+            // 注册 Boss 信息
+            bossChecklist.Call(
+                "LogBoss",
+                Mod,
+                internalName,
+                weight,
+                downed,
+                bossType,
+                new Dictionary<string, object>()
+                {
+                    //["spawnItems"] = spawnItem,// 召唤物品
+                    ["displayName"] = Language.GetText("金熊王"),// 显示名称
+                    //["spawnInfo"] = Language.GetText(""),// 召唤信息
+                    //["collectibles"] = collectibles,// 收集物品
+                    ["customPortrait"] = customPortrait// 自定义图标显示法
+
+                }
+            );
+            //自定义图标显示法
+            var customPortrait1 = (SpriteBatch sb, Rectangle rect, Color color) =>
+            {
+                Texture2D texture = ModContent.Request<Texture2D>("AncientChineseMythology/Textures/BlackBear/BlackBear").Value;
+                Vector2 centered = new Vector2(rect.X + (rect.Width / 2) - (texture.Width / 2), rect.Y + (rect.Height / 2) - (texture.Height / 2));
+                sb.Draw(texture, centered, color);
+            };
+        }
+    }
+
+    public class DownedBossSystem : ModSystem
+    {
+        public static bool downedBlackBear = false; // 跟踪 BlackBear 是否已被击败
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            tag["downedBlackBear"] = downedBlackBear; // 保存状态
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            downedBlackBear = tag.GetBool("downedBlackBear"); // 加载状态
+        }
+
+        public override void OnWorldLoad()
+        {
+            // 重置所有 Boss 的击败状态
+            downedBlackBear = false;
+        }
     }
 }
