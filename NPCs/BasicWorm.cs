7efca400eadfbad781d73c77495436f9f5dd32a7
纸草 <papyrus_123@163.com>
2025-05-12 09:45:02 +0800

    添加蠕虫基类

---
+++ NPCs/BasicWorm.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace AncientChineseMythology.NPCs
+{
+    /// <summary>
+    /// 摘要：NPC的width是虫子距离上一节的距离
+    /// </summary>
+    public abstract class BasicWorm : ModNPC
+    {
+        public enum WormType : byte
+        {
+            Head,
+            Body,
+            Tail
+        }
+        public override void SetDefaults() // 子类保留这个的Base以方便调用父类的修改AIStyle与AIType
+        {
+            NPC.aiStyle = -1; // 这是为了避免NPC的AI类型与原版有所冲突
+        }
+        /// <summary>
+        /// 启用SPDir翻转
+        /// </summary>
+        public virtual bool IsUseSpriteDirection => false;
+        public abstract WormType NPCWormType { get; }
+        /// <summary>
+        /// 上一个Worm
+        /// </summary>
+        public int FatherWorm = -1;
+        public NPC FatherNPC
+        {
+            get
+            {
+                if(FatherWorm != -1)
+                    return Main.npc[FatherWorm];
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 时候生成了下一节NPC
+        /// </summary>
+        public bool IsSummonNPC;
+        /// <summary>
+        /// 召唤NPC的Type,请在SetDefaults中设置,也可以根据SummonCount在ChangeSummonType修改
+        /// </summary>
+        public int SummonNPCType;
+        /// <summary>
+        /// 召唤NPC的时间
+        /// </summary>
+        public int SummonTime;
+        /// <summary>
+        /// 召唤上限,请在SetDefaults中设置
+        /// </summary>
+        public int SummonMax;
+        /// <summary>
+        /// 召唤次数
+        /// </summary>
+        public int SummonCount;
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(FatherWorm);
+            #region 通过这个传输Bool
+            BitsByte bitsByte = new BitsByte();
+            bitsByte[0] = IsSummonNPC;
+            writer.Write(bitsByte);
+            #endregion
+        }
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            FatherWorm = reader.ReadInt32();
+            #region 通过这个传输Bool
+            BitsByte bitsByte = reader.ReadByte();
+            IsSummonNPC = bitsByte[0];
+            #endregion
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            if(source is EntitySource_Parent parent && parent.Entity is NPC npc)
+            {
+                FatherWorm = npc.whoAmI;
+                SummonTime = 5;
+                if(npc.ModNPC is BasicWorm basicWorm)
+                {
+                    SummonMax = basicWorm.SummonMax;
+                    SummonCount = basicWorm.SummonCount + 1;
+                }
+
+                while (npc.ModNPC is BasicWorm basicWorm1 && basicWorm1.FatherWorm != -1) // 找到头部
+                {
+                    npc = Main.npc[basicWorm1.FatherWorm]; // 迭代NPC
+                }
+                if(npc.active && npc.ModNPC is BasicWorm)
+                    NPC.realLife = npc.whoAmI;
+            }
+        }
+        /// <summary>
+        /// 有事没事不要重写这个，这个是根据NPCWormType来确定NPC的AI
+        /// </summary>
+        public override void PostAI()
+        {
+            switch (NPCWormType)
+            {
+                case WormType.Head:
+                    if(!IsSummonNPC)
+                    {
+                        ChangeSummonType();
+                        IsSummonNPC = true;
+                        NPC.netUpdate = true;
+                        NPC.NewNPCDirect(NPC.GetSource_FromThis(), NPC.position, SummonNPCType, NPC.whoAmI);
+                    }
+                    break;
+                case WormType.Body:
+                case WormType.Tail:
+                    if(SummonTime > 0)
+                        SummonTime--;
+                    if (!IsSummonNPC && SummonTime <= 0 && SummonMax >= SummonCount)
+                    {
+                        ChangeSummonType();
+                        IsSummonNPC = true;
+                        NPC.netUpdate = true;
+                        NPC.NewNPCDirect(NPC.GetSource_FromThis(), NPC.position, SummonNPCType, NPC.whoAmI);
+                    }
+                    if (FatherNPC != null)
+                        ChangePos();
+                    if (FatherNPC?.active == false)
+                        NPC.active = false;
+                    NPC.rotation = NPC.velocity.ToRotation();
+                    if (IsUseSpriteDirection)
+                    {
+                        if (NPCWormType == WormType.Tail && FatherNPC != null)
+                            NPC.spriteDirection = FatherNPC.spriteDirection;
+                        else
+                            NPC.spriteDirection = NPC.velocity.X > 0 ? 1 : -1; // 自动修改朝向
+                    }
+                    if (NPC.spriteDirection == -1)
+                        NPC.rotation += MathHelper.Pi;
+                    break;
+            }
+        }
+        /// <summary>
+        /// 可以重写这个方法来改变位置算法
+        /// </summary>
+        public virtual void ChangePos()
+        {
+            NPC.velocity = Vector2.Lerp(NPC.rotation.ToRotationVector2() * NPC.spriteDirection, FatherNPC.rotation.ToRotationVector2() * FatherNPC.spriteDirection, 0.5f);
+            NPC.Center = FatherNPC.Center - NPC.velocity * (NPC.width + FatherNPC.width) / 2;
+        }
+        /// <summary>
+        /// 修改生成的NPC类型
+        /// </summary>
+        public virtual void ChangeSummonType()
+        {
+
+        }
+    }
+}
---
+++ NPCs/Boss/Archosaur/ArchosaurBoss.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace AncientChineseMythology.NPCs.Boss.Archosaur
+{
+    public abstract class ArchosaurBoss : BasicWorm
+    {
+        public override string Texture => "AncientChineseMythology/Textures/NPCs/Boss/Archosaur/" + Name;
+        public override bool IsUseSpriteDirection => true;
+        public Player Target
+        {
+            get
+            {
+                if (NPC.target < 0 || NPC.target >= Main.maxPlayers || Main.player[NPC.target].dead || !Main.player[NPC.target].active)
+                    NPC.TargetClosest();
+                return Main.player[NPC.target];
+            }
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.height = 10;
+            NPC.lifeMax = 100000;
+            NPC.damage = 150;
+            NPC.defense = 30;
+            NPC.boss = true;
+            NPC.noTileCollide = true;
+            NPC.noGravity = true;
+            NPC.knockBackResist = 0;
+            SummonMax = 25;
+        }
+        public override void AI()
+        {
+            base.AI();
+            if (NPCWormType == WormType.Head) // 头部执行AI
+            {
+                Vector2 vel = Target.Center - NPC.Center; // 目标方向
+                NPC.rotation = NPC.velocity.ToRotation();
+                NPC.spriteDirection = NPC.velocity.X <= 0 ? -1 : 1;
+                if (NPC.spriteDirection == -1)
+                    NPC.rotation += MathHelper.Pi;
+                if (vel.Length() > 300)
+                {
+                    Vector2 changeVel = vel.SafeNormalize(Vector2.UnitX); // 改变的速度
+                    NPC.velocity = (NPC.velocity * 50 + changeVel * 32) / 51f; // 修改速度
+                }
+                else
+                    NPC.velocity *= 1.01f;
+            }
+        }
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            _ = TextureAssets.Npc[Type].Value;
+            Texture2D tex = TextureAssets.Npc[Type].Value;
+            Vector2 origin = new(NPC.spriteDirection == -1 ? 0 : tex.Width, 20);
+            if (NPCWormType == WormType.Head) // 头部执行AI
+            {
+                origin.Y += 34;
+                origin.X = NPC.spriteDirection == -1 ? (tex.Width / 4) : (tex.Width / 4 * 3);
+            }
+            spriteBatch.Draw(tex, NPC.Center - screenPos, null, drawColor, NPC.rotation, origin, NPC.scale, NPC.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+            return false;
+        }
+    }
+    public class ArchosaurHead : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Head;
+        public override void ChangeSummonType()
+        {
+            SummonNPCType = ModContent.NPCType<ArchosaurBody2>();
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 50;
+        }
+    }
+    public class ArchosaurBody1 : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Body;
+        public override void ChangeSummonType()
+        {
+            SummonNPCType = ModContent.NPCType<ArchosaurBody2>();
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 15;
+            NPC.height = 50;
+        }
+    }
+    public class ArchosaurBody2 : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Body;
+        public override void ChangeSummonType()
+        {
+            SummonNPCType = ModContent.NPCType<ArchosaurBody2>();
+            if (SummonCount == SummonMax / 3 * 2 || SummonCount == 3)
+                SummonNPCType = ModContent.NPCType<ArchosaurBody1>();
+            if(SummonCount > SummonMax - 3)
+                SummonNPCType = ModContent.NPCType<ArchosaurBody3>();
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 15;
+        }
+    }
+    public class ArchosaurBody3 : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Body;
+        public override void ChangeSummonType() => SummonNPCType = ModContent.NPCType<ArchosaurBody4>();
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 20;
+        }
+    }
+    public class ArchosaurBody4 : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Body;
+        public override void ChangeSummonType() => SummonNPCType = ModContent.NPCType<ArchosaurTail>();
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 20;
+        }
+    }
+    public class ArchosaurTail : ArchosaurBoss
+    {
+        public override WormType NPCWormType => WormType.Tail;
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            NPC.width = 20;
+        }
+    }
+}
