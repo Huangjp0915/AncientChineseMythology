5e301949af8e5268cc60a9109c08f2c8f809ced3
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-12 17:22:52 +0800

    update

--- Underworlds/Underworld.cs
+++ Underworlds/Underworld.cs
@@ -1,4 +1,5 @@
 ﻿using AncientChineseMythology.Underworlds.Tiles;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria.ModLoader;
 
 namespace AncientChineseMythology.Underworlds
@@ -6,7 +7,8 @@ namespace AncientChineseMythology.Underworlds
     internal class Underworld : ModSystem
     {
         public static int UmbralStoneTileID => ModContent.TileType<UmbralStone>();
-
+        [VaultLoaden("{@namespace/}")]
+        public static Texture2D Fog;//反射加载雾气灰度纹理
         /// <summary>
         /// 生成地府地形的接口方法
         /// 可在任何时候调用以生成地府地形
