3872f4d5c37c4c5498aaf602ec91bd4b4c912cd4
HoCha113 <127948319+hocha113@users.noreply.github.com>
2026-01-01 23:57:13 +0800

    update

---
+++ NPCs/Boss/KyuubiKitsunes/KyuubiKitsune.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace AncientChineseMythology.NPCs.Boss.KyuubiKitsunes
+{
+    internal class KyuubiKitsune
+    {
+        [VaultLoaden("{@namespace}/")]
+        public static Texture2D MissesBody;
+        [VaultLoaden("{@namespace}/")]
+        public static Texture2D MissesTop;
+    }
+}
